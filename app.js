aefd58fc3cf042a764bde47c2d03c4e24949ab2b
Sumith <poone@DESTOP-AIQ6SBS.local>
2022-06-30 22:30:08 +0530

    added procfile, gitignore and server updated

--- app.js
+++ app.js
@@ -72,7 +72,10 @@ app.get("/contact", function(req, res){
   res.render("contact", {contactContent: contactContent});
 });
 
-
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
+app.listen(port, function() { 
+  console.log("Server started successfully");
 });
